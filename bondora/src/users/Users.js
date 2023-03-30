cf8ce3125cce9f172f9634d6e1d63f9058b63d61
Jacob <jaagup.tomingas@gmail.com>
2023-03-30 12:55:56 +0300

    users xd

---
+++ bondora/src/users/Users.js
@@ -0,0 +1,14 @@
+
+
+const USERS = {
+    "Annika": "bank-statement-1-clean",
+    "Raigo": "bank-statement-2-clean",
+    "Rait": "bank-statement-3-clean",
+    "Lauri": "bank-statement-4-clean",
+    "Jaagup": "bank-statement-5-clean",
+    "Juhan": "bank-statement-6-clean",
+    "Samuel": "bank-statement-7-clean",
+    "Karl": "bank-statement-8-clean",
+    "Stefan": "bank-statement-9-clean",
+    "Katariina": "bank-statement-10-clean",
+};
